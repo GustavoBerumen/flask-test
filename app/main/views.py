b502ab7969ab2ad9f704e5e624c8505c39fbaf6e
GustavoBerumen <lpxjgb@nottingham.ac.uk>
2020-04-14 19:48:24 +0100

    Changes to be committed: 	modified:   app/main/__pycache__/views.cpython-36.pyc 	modified:   app/main/views.py 	modified:   app/templates/base.html 	new file:   app/templates/basic.html 	modified:   app/templates/index.html 	new file:   app/templates/instructions.html 	new file:   app/templates/privacy.html 	modified:   venv/bin/activate 	modified:   venv/bin/activate.csh 	modified:   venv/bin/activate.fish

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from . import main
 
-
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/privacy/', methods=['POST'])
+def privacy():
+    return render_template('privacy.html'
+
+@main.route('/basic/', methods=['POST'])
+def basic():
+    return render_template('basic.html'))
+
+@main.route('/instructions/', methods=['POST'])
+def instructions():
+    return render_template('instructions.html')
